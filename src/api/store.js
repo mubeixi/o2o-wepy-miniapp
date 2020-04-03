0ba34d90c53e14ccafa51620bcd8dee3d0f6890f
mbx0829@163.com <mubeixi0829>
2020-04-03 12:45:42 +0800

    修复优惠券添加和编辑页面

--- src/api/store.js
+++ src/api/store.js
@@ -33,3 +33,13 @@ export const bizRegSms = (param, options) => fetch({act: 'bizRegSms', param, opt
 // 获取城市列表
 export const getCityList = (param, options) => fetch({act: 'getCityList', param, options})
 
+
+// 获取会员等级列表
+export const getUserLevel = (param, options) => fetch({act: 'getUserLevel', param, options})
+
+// 获取优惠券列表
+export const getCouponList = (param, options) => fetch({act: 'getCouponList', param, options})
+
+// 获取优惠券列表
+export const opCoupon = (param, options) => fetch({act: 'opCoupon', param, options})
+
