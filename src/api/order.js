c5523ba8e909d252570816f23f6d688e2b59db24
mbx0829@163.com <mubeixi0829>
2020-04-14 18:10:21 +0800

    fix(商家订单): 新增商家订单列表页面和接口
    1.新增商家订单列表页面和接口2.新增商家订单发货页面接口

---
+++ src/api/order.js
@@ -0,0 +1,17 @@
+import { fetch } from '../common/request'
+
+// 订单列表
+export const getOrder = (param, options) => fetch({act: 'get_order', param, options})
+
+// 拒单
+export const systemRejectOrder = (param, options) => fetch({act: 'system_reject_order', param, options})
+
+
+// 订单详情
+export const getOrderDetail = (param, options) => fetch({act: 'get_order_detail', param, options})
+
+// 获取合作公司
+export const getAvailableCityExpress = (param, options) => fetch({act: 'getAvailableCityExpress', param, options})
+
+// 获取合作公司
+export const systemSendOrder = (param, options) => fetch({act: 'system_send_order', param, options})
