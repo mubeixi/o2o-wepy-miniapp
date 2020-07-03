82f8bb186def3e7184dda88f1181dae40882c493
junxiangwang <271938312@qq.com>
2020-07-03 11:41:30 +0800

    fix(合并修改): 合并修改

--- src/common/env.js
+++ src/common/env.js
@@ -1,5 +1,5 @@
 // export const apiBaseUrl = 'http://www.newo2osrc.com'
-export const apiBaseUrl = 'https://newo2o.netcnnet.net'
+export const apiBaseUrl = 'https://newo2o.bafangka.com'
 export const staticUrl = 'https://newo2o.bafangka.com'
 
 //测试appid   wx3d24c565489e305b     正式商户id   wxfcabc14aef501fdd   正式域名https://newo2o.netcnnet.net
