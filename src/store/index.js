f1a4b0dde25bbbb40ff764f2371da42f2ab208ae
mbx0829@163.com <mubeixi0829>
2020-07-25 09:55:21 +0800

    1

--- src/store/index.js
+++ src/store/index.js
@@ -36,7 +36,7 @@ export default new Vuex.Store({
     },
     SET_INIT_INFO(state, val) {
       state.initInfo = val
-      ls.set('initInfo', val, 1)cash_from
+      ls.set('initInfo', val, 1)
     },
     increment(state) {
       state.counter++
